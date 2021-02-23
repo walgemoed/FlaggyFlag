2bc671b1478c2d025c05d48b7ee8884dbf8e29c7
Henk Walgemoed <henk@walgemoed.com>
2021-02-23 12:16:29 +0100

    json php

---
+++ cal.php
@@ -0,0 +1,13 @@
+<?php
+$jsonBody = '['
+        . '{'
+        . '"name" : "Verjaardag prinses Beatrix (1938)"'
+        . '"description" : "Wimpel: ✅\nHalfstok: ❌"'
+        . '"day" : "31"'
+        . '"month" : "01"'
+        . '"dayreplacement" : "01"'
+        . '"monthreplacement" : "02"'
+        . '}'
+        . ']';
+
+echo $jsonBody;
